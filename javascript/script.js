c9a098ef77d832426c1731aa61b278e983a0b3bc
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-02-12 15:41:29 -0300

    Update script.js

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+const passwordIcons = document.querySelectorAll('.password-icon');
+
+passwordIcons.forEach(icon => {
+    icon.addEventListener('click', function() {
+        const input = this.parentElement.querySelector('.form-control')
+    })
+})
